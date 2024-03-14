05f6543cd54be3ec340c987c396d25f9faaf8c72
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-03-14 08:03:12 +0200

    modified non responsive elements

--- pages/pages.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("loginButton").onclick = function() {
-    applyAnimation();
-    alert("Button clicked!");
-    document.getElementById("hiddenElement").style.display = "block";
-};
