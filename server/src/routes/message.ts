6f5dd52a47bcf9e2e01a9e16c26e28e4fae41e82
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-04-02 14:01:45 +0200

    corrected admin routes

--- server/src/routes/message.ts
+++ server/src/routes/message.ts
@@ -1,16 +1,17 @@
 import { Router } from "express"
 import { getAllMessages, createMessage, getSingleMessage, deleteMessage } from '../controllers/message'
 import { validateMessage } from '../middleware/messageValidation';
+import { adminAuthJWT, userAuthJWT } from '../middleware/auth';
 
 
 const router: Router = Router()
 
-router.get("/", getAllMessages)
+router.get("/", adminAuthJWT, getAllMessages)
 
 router.post("/", validateMessage, createMessage)
 
 router.get("/:id", getSingleMessage)
 
-router.delete("/:id", deleteMessage)
+router.delete("/:id", adminAuthJWT, deleteMessage)
 
 export default router
