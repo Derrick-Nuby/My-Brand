c2f94f640af84fccb7536a5cd23a4b6fc2d238ec
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-04-13 16:33:33 +0200

    removed te before each

--- server/src/test/auth.test.ts
+++ server/src/test/auth.test.ts
@@ -9,54 +9,54 @@ describe('Authentication API Tests', () => {
 
   describe('Create Account', () => {
 
-    beforeEach(async () => {
-      await User.deleteMany({});
+    // beforeEach(async () => {
+    //   await User.deleteMany({});
+    // });
+
+    it('should create a new user account with valid input', async () => {
+      const userData = {
+        name: '222User',
+        phone: '0786263290',
+        email: '222user@gmail.com',
+        password: 'Passw0RD!',
+        confirmPassword: 'Passw0RD!',
+      };
+
+      const res = await request(app)
+        .post('/api/user/create')
+        .send(userData);
+
+      expect(res.status).to.equal(201);
+      expect(res.body).to.have.property('message').equal('User Created Successfully');
+      expect(res.body).to.have.property('user');
+      expect(res.body.user).to.have.property('name').equal(userData.name);
+      expect(res.body.user).to.have.property('email').equal(userData.email);
     });
 
-    // it('should create a new user account with valid input', async () => {
-    //   const userData = {
-    //     name: '222User',
-    //     phone: '0786263290',
-    //     email: '222user@gmail.com',
-    //     password: 'Passw0RD!',
-    //     confirmPassword: 'Passw0RD!',
-    //   };
+    it('should return an error if the email is already in use', async () => {
+      const existingUser = new User({
+        name: '222User',
+        phone: '0786263290',
+        email: '222user@gmail.com',
+        password: await bcrypt.hash('Passw0RD!', 10),
+      });
+      await existingUser.save();
 
-    //   const res = await request(app)
-    //     .post('/api/user/create')
-    //     .send(userData);
+      const userData = {
+        name: '222User',
+        phone: '0786263290',
+        email: '222user@gmail.com',
+        password: 'Passw0RD!',
+        confirmPassword: 'Passw0RD!',
+      };
 
-    //   expect(res.status).to.equal(201);
-    //   expect(res.body).to.have.property('message').equal('User Created Successfully');
-    //   expect(res.body).to.have.property('user');
-    //   expect(res.body.user).to.have.property('name').equal(userData.name);
-    //   expect(res.body.user).to.have.property('email').equal(userData.email);
-    // });
+      const res = await request(app)
+        .post('/api/user/create')
+        .send(userData);
 
-    // it('should return an error if the email is already in use', async () => {
-    //   const existingUser = new User({
-    //     name: '222User',
-    //     phone: '0786263290',
-    //     email: '222user@gmail.com',
-    //     password: await bcrypt.hash('Passw0RD!', 10),
-    //   });
-    //   await existingUser.save();
-
-    //   const userData = {
-    //     name: '222User',
-    //     phone: '0786263290',
-    //     email: '222user@gmail.com',
-    //     password: 'Passw0RD!',
-    //     confirmPassword: 'Passw0RD!',
-    //   };
-
-    //   const res = await request(app)
-    //     .post('/api/user/create')
-    //     .send(userData);
-
-    //   expect(res.status).to.equal(400);
-    //   expect(res.body).to.have.property('error').equal('A user with that email already exists, if that is you.. please login or reset your password');
-    // });
+      expect(res.status).to.equal(400);
+      expect(res.body).to.have.property('error').equal('A user with that email already exists, if that is you.. please login or reset your password');
+    });
   });
 
   describe('User Login', () => {
