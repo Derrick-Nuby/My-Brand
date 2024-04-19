98e7d65b4e56d4ed085991598f61b9b164adfa38
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-04-19 11:21:58 +0200

    allowed all origins

--- server/src/app.ts
+++ server/src/app.ts
@@ -23,7 +23,7 @@ const PORT: string | number = process.env.PORT || 4000
 
 // app.use(local)
 app.use(cors({
-  origin: ['http://localhost:4000', 'http://127.0.0.1:5500', 'http://localhost:5500/', 'https://derrick-nuby.github.io', 'https://derricks-portfolio.netlify.app'],
+  origin: [ '*', 'http://127.0.0.1:5500', 'https://derrick-nuby.github.io', 'https://derricks-portfolio.netlify.app'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
