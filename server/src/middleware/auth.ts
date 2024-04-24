cad8b6d594d65e5e5f41f7e22e9618e7523ef2f5
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-04-24 12:54:20 +0200

    message changed to error

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -83,7 +83,7 @@ const adminAuthJWT = (req: Request, res: Response, next: NextFunction) => {
       req.isAdmin = isAdmin;
 
       if (!isAdmin) {
-        return res.status(403).json({ message: 'You are not allowed to access this resource, Please contact the site administrator for assistance' });
+        return res.status(403).json({ error: 'You are not allowed to access this resource, Please contact the site administrator for assistance' });
       }
       next();
     });
@@ -92,4 +92,85 @@ const adminAuthJWT = (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { userAuthJWT, adminAuthJWT };
+const loggedUserAuth = (req: Request, res: Response, next: NextFunction) => {
+
+  let token: string | undefined;
+
+  const authorizationHeader = req.headers.authorization;
+  
+  
+  if (authorizationHeader) {
+      const parts = authorizationHeader.split(' ');
+      if (parts.length === 2 && parts[0] === 'Bearer') {
+          token = parts[1];
+      }
+  } 
+  if (!token) {
+    token = req.cookies.jwt;
+  }
+  if (!token) {
+    res.status(401).json({ error: 'Authentication required. Please log in.' });
+    return;
+  }
+  
+  if (token) {
+    jwt.verify(token, 'jwtSecret', (err: any, decoded: any) => {
+      if (err) {
+        return res.status(403).json({ error: 'Failed to authenticate token, Please Login again' });
+      } else {
+      const { id, username, isAdmin } = decoded;
+      req.userId = id;
+      req.lUsername = username;
+      req.isAdmin = isAdmin;
+      
+      return res.status(200).json({ message: `You are allowed to access this resource as you are logged in as ${username} User`})
+    }
+
+    });
+  } else {
+    return res.status(401).json({ error: 'You need to login to access this resource; Please login or create an account' });
+  }
+};
+
+const loggedAdminAuth = (req: Request, res: Response, next: NextFunction) => {
+
+  let token: string | undefined;
+
+  const authorizationHeader = req.headers.authorization;
+  
+  if (authorizationHeader) {
+      const parts = authorizationHeader.split(' ');
+      if (parts.length === 2 && parts[0] === 'Bearer') {
+          token = parts[1];
+      }
+  } 
+  if (!token) {
+    token = req.cookies.jwt;
+  }
+  if (!token) {
+    return res.status(401).json({ error: 'Authentication required. Please log in.' });
+  }
+
+  if (token) {
+    jwt.verify(token, 'jwtSecret', (err: any, decoded: any) => {
+      if (err) {
+        return res.status(403).json({ error: 'Failed to authenticate token, Please Login again' });
+      }
+      const { id, username, isAdmin } = decoded;
+      req.userId = id;
+      req.lUsername = username;
+      req.isAdmin = isAdmin;
+
+      if (isAdmin) {
+        return  res.status(200).json({ message: `You are allowed to access this resource as you are logged in as ${username} Admin`})
+      } else {
+        return res.status(403).json({ error: 'You are not allowed to access this resource, Please contact the site administrator for assistance' });
+      }
+      
+    });
+  } else {
+    res.status(401).json({ error: 'You need to login to access this resource; Please login or create an account' });
+  }
+};
+
+export { userAuthJWT, adminAuthJWT, loggedUserAuth, loggedAdminAuth };
